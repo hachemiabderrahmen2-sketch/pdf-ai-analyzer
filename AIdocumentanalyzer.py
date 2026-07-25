f30a50f7633273d469be24ca38d4f0596fc113a6
hachemiabderrahmen2-sketch <hachemiabderrahmen2@gmail.com>
2026-07-25 16:03:28 +0100

    Add files via upload

--- AIdocumentanalyzer.py
+++ AIdocumentanalyzer.py
@@ -8,7 +8,11 @@ st.title("📁 AI Contract & Document Analyzer")
 st.write("Upload any PDF document (contract, guide, report) and ask questions to get instant, accurate answers!")
 import os
 
-api_key = st.secrets.get("GEMINI_API_KEY")
+api_key = st.secrets.get("GEMINI_API_KEY", "")
+if not api_key:
+    st.error("Gemini API Key is missing! Please configure it in Streamlit Secrets.")
+else:
+    client = genai.Client(api_key=api_key)
 
 uploaded_file = st.file_uploader ("choose a PDF file", type = ["PDF"])
 
@@ -27,13 +31,13 @@ if uploaded_file is not None :
         if user_query: 
             with st.spinner("Analyzing document and generating response..."): 
                 try:
-                    model = genai.GenerativeModel('gemini-1.5-flash')
+    
                     prompt =f"""You are an expert document analysis assistant. Based ONLY on the provided document text below, answer the user's question accurately and concisely. If the answer is not contained within the text, explicitly state: " This information is not available in the provided document."
                     --- START DOCUMENT TEXT ---
                     {extracted_text[:10000]}
                     --- END DOCUMENT TEXT --- 
                     User Question: {user_query}"""
-                    response =model.generate_content(prompt)
+                    response =client.models.generate_content(model='gemini-2.5-flash', contents=prompt,)
                     st.markdown("### 🤖 response:")
                     st.write(response.text)
 
